89d9c6423363365e5c0679d9995d39bd54f8e85b
Braulio <bgrana@conwet.com>
2016-05-24 10:37:58 +0200

    Remove subsetting

--- trainingAnalysis.R
+++ trainingAnalysis.R
@@ -4,7 +4,6 @@
 #fdat = read.csv("~/sngular/peopleAnalytics/employee-training.csv")
 fdat = read.csv("/Users/Hugo/Documents/EIT Master/SingularMeaning/peopleanalytics/employee-training.csv")
 # Clean data
-fdat = subset(fdat, select = -c(Position.count, Planned.position.count, Terminations, Internal.hires, External.hires, Department, Organization))
 fdat = na.omit(fdat)
 
 ## See data types
@@ -94,8 +93,8 @@ assign_course_online <- function(perc) {
   return(ifelse(runif(1, 0.0, 1.0)<=perc, "Yes", "No"))
 }
 
-#Graphs
 
+# Plots
 ggplot(fdat,  aes(x = Course.name, fill = Course.online)  ) + geom_bar() + xlab('Course.name') + ylab('Count')
 
 
