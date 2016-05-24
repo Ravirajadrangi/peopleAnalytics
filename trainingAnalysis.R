845e1013a0b93b99655dd4edf259111b9148d1fc
Braulio <bgrana@conwet.com>
2016-05-24 10:18:53 +0200

    Add position and attrition based on position

--- trainingAnalysis.R
+++ trainingAnalysis.R
@@ -23,6 +23,11 @@ str(fdat)
 #2. Sales Manager -> 8.5%
 #3. Sales Representatives 18.1%
 
+# Attrition according to position:
+#1. Sales Executive -> 17.48%
+#2. Sales Manager -> 5.4%
+#3. Sales Representatives 39.75%
+
 #Courses Names:
 #1. Course A
 #2. Course B
@@ -42,22 +47,6 @@ str(fdat)
 # -/+ %
 #-----------------------------------------------
 
-# Factors
-
-
-# Functions
-assign_attrition <- function(perc) {
-  return(ifelse(runif(1, 0.0, 1.0)<=perc, "Yes", "No"))
-}
-
-fdat$Attrition = sapply(fdat$Course.days, function(days) {
-  ifelse(days > 5, assign_attrition(0.05), assign_attrition(0.95))
-})
-
-ggplot(fdat, aes(x = Course.days)) + geom_bar(aes(fill = Attrition), position = 'fill') +
-  scale_y_continuous(labels = percent_format())
-
-
 fdat$Course.name = sapply(fdat$Course.days, function(days) {
   ifelse(days > 5, assign_course_name(0.50), assign_course_name(0.50))
 })
@@ -66,6 +55,29 @@ fdat$Course.online = sapply(fdat$Course.days, function(days) {
   ifelse(days > 5, assign_course_online(0.46), assign_course_online(0.54))
 })
 
+fdat$Position = sapply(fdat$Position, function(position) {
+  return(assign_position())
+})
+
+fdat$Attrition = sapply(fdat$Position, function(position) {
+  ifelse(position == "Manager", assign_attrition(0.054),
+         ifelse(position == "Executive", assign_attrition(0.1748), assign_attrition(0.3975)))
+})
+
+# Factors
+
+
+# Functions
+assign_attrition <- function(perc) {
+  return(ifelse(runif(1, 0.0, 1.0)<=perc, "Yes", "No"))
+}
+
+assign_position <- function() {
+  perc = runif(1, 0.0, 1.0)
+  return(ifelse(perc <= 0.085, "Manager",
+                ifelse(perc <= 0.181, "Representative", "Executive")))
+}
+
 assign_course_name <- function(perc) {
   return(ifelse(runif(1, 0.0, 1.0)<=perc, "Course A", "Course B"))
 }
@@ -73,3 +85,11 @@ assign_course_name <- function(perc) {
 assign_course_online <- function(perc) {
   return(ifelse(runif(1, 0.0, 1.0)<=perc, "Yes", "No"))
 }
+
+
+# Plots
+ggplot(fdat, aes(x = Position)) + geom_bar(aes(fill = Attrition), position = 'fill') +
+  scale_y_continuous(labels = percent_format())
+
+# Update csv file
+write.csv(fdat, '~/sngular/peopleAnalytics/employee-training.csv')
